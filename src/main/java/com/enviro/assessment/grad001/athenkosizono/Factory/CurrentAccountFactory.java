65b1cce05bc01e5e43cd41f4e7a289314cfcf510
Athenkosi <athenkosizono@gmail.com>
2022-04-28 17:49:53 +0200

    Added Factory classes

--- src/main/java/com/enviro/assessment/grad001/athenkosizono/Factory/CurrentAccountFactory.java
+++ src/main/java/com/enviro/assessment/grad001/athenkosizono/Factory/CurrentAccountFactory.java
@@ -5,7 +5,7 @@ import com.enviro.assessment.grad001.athenkosizono.Domain.CurrentAccount;
 public class CurrentAccountFactory {
 
     //Creating a new Instance of Current Account
-    public static CurrentAccount createCurrentAccount(int id, long accountNum, double balance, double overdraft){
+    public static CurrentAccount createCurrentAccount(int id, String accountNum, double balance, double overdraft){
         return new CurrentAccount(id, accountNum, balance, overdraft);
     }
 }
--- src/main/java/com/enviro/assessment/grad001/athenkosizono/Factory/SavingsAccountFactory.java
+++ src/main/java/com/enviro/assessment/grad001/athenkosizono/Factory/SavingsAccountFactory.java
@@ -4,7 +4,7 @@ import com.enviro.assessment.grad001.athenkosizono.Domain.SavingsAccount;
 
 public class SavingsAccountFactory {
 
-    public static SavingsAccount createSavingsAccount(int id, long accountNum, double balance){
+    public static SavingsAccount createSavingsAccount(int id, String accountNum, double balance){
         return new SavingsAccount(id, accountNum, balance);
     }
 }
