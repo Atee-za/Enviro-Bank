6591a80ce87bc0d83c30ceafc3460bac2f1747d5
Athenkosi <athenkosizono@gmail.com>
2022-04-28 17:40:31 +0200

    Added Factory tests

---
+++ src/test/java/com/enviro/assessment/grad001/athenkosizono/Factory/SavingsAccountFactoryTest.java
@@ -0,0 +1,18 @@
+package com.enviro.assessment.grad001.athenkosizono.Factory;
+
+import com.enviro.assessment.grad001.athenkosizono.Domain.SavingsAccount;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SavingsAccountFactoryTest {
+
+    private static SavingsAccount savingsAccount = SavingsAccountFactory.createSavingsAccount(110,255552,550);
+
+    @Test
+    void createSavingsAccount() {
+        assertNotNull(savingsAccount);
+        assertEquals(550, savingsAccount.getBalance());
+        System.out.println(savingsAccount);
+    }
+}
