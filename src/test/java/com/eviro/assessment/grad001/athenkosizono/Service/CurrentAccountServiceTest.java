cb502cae5b0a76739599dae93669026047b341be
Athenkosi <athenkosizono@gmail.com>
2022-04-29 12:34:33 +0200

    Added Service layer test cases for Savings and Current accounts

---
+++ src/test/java/com/eviro/assessment/grad001/athenkosizono/Service/CurrentAccountServiceTest.java
@@ -0,0 +1,72 @@
+package com.eviro.assessment.grad001.athenkosizono.Service;
+
+import com.eviro.assessment.grad001.athenkosizono.Domain.CurrentAccount;
+import com.eviro.assessment.grad001.athenkosizono.Domain.Database.SystemDB;
+import com.eviro.assessment.grad001.athenkosizono.Factory.CurrentAccountFactory;
+
+import java.math.BigDecimal;
+
+class CurrentAccountServiceTest {
+
+    public static void main(String[] args) throws Exception {
+        new CurrentAccountServiceTest().runAllCurrentAccountTests();
+    }
+
+    private static CurrentAccountService service = CurrentAccountService.getCurrentAccountService();
+    private CurrentAccount currentAccount = CurrentAccountFactory.createCurrentAccount("1111",108,"123X5Y", BigDecimal.valueOf(5000), BigDecimal.valueOf(6000));
+    private CurrentAccount currentAccount1 = CurrentAccountFactory.createCurrentAccount("8544",150,"PR454", BigDecimal.valueOf(-2000), BigDecimal.valueOf(8000));
+
+    private static SystemDB systemDB = SystemDB.getSystemDB();
+
+    //To check if Singleton if functioning correctly
+    private static SystemDB systemDB2 = SystemDB.getSystemDB();
+
+    private void runAllCurrentAccountTests() throws Exception {
+        // Adding Accounts to database
+        systemDB.add(currentAccount);
+        systemDB.add(currentAccount1);
+
+        currentAccountWithdraw();
+        testExceededBalance();
+        testWithdrawNotFound();
+        testingSingleton();
+    }
+
+    private void currentAccountWithdraw() throws Exception {
+        service.withdraw("123X5Y", BigDecimal.valueOf(10000));
+        System.out.println("Test Pass {Current Account with account number 123X5Y successfully executed.}");
+    }
+
+    private void testExceededBalance(){
+        // Making balance exceed maximum overdraft limit, expecting WithdrawalAmountTooLargeException
+        try{
+            service.withdraw("PR454",BigDecimal.valueOf(7000));
+        }
+        catch (Exception e){
+            System.out.println("Test Pass {" + e.getMessage() + ".}");
+        }
+    }
+
+    private void testWithdrawNotFound() {
+        // Finding none existing account expecting AccountNotFoundException
+        try{
+            service.withdraw("XXX",BigDecimal.valueOf(2002));
+            System.out.println("Test Fail {Withdrawal was success.}");
+        }
+        catch (Exception e){
+            System.out.println("Test Pass {" + e.getMessage() + ".}");
+        }
+    }
+
+    private void testingSingleton(){
+        // Checking if database has only 1 instance
+        try{
+            systemDB2.withdraw("PR454",BigDecimal.valueOf(1000));
+            System.out.println("Test Pass {Accounts in SystemDB are in SystemDB2.}");
+        }
+        catch (Exception e){
+            System.out.println("Test Fail {Other Accounts are not found.}");
+        }
+    }
+
+}
---
+++ src/test/java/com/eviro/assessment/grad001/athenkosizono/Service/SavingsAccountServiceTest.java
@@ -0,0 +1,78 @@
+package com.eviro.assessment.grad001.athenkosizono.Service;
+
+import com.eviro.assessment.grad001.athenkosizono.Domain.Database.SystemDB;
+import com.eviro.assessment.grad001.athenkosizono.Domain.SavingsAccount;
+import com.eviro.assessment.grad001.athenkosizono.Factory.SavingsAccountFactory;
+
+import javax.security.auth.login.AccountNotFoundException;
+import java.math.BigDecimal;
+
+class SavingsAccountServiceTest {
+
+    private static SavingsAccountService service = SavingsAccountService.getSavingsAccountService();
+    private SavingsAccount savingsAccount = SavingsAccountFactory.createSavingsAccount("1111",108,"123X5Y", BigDecimal.valueOf(5000));
+    private SavingsAccount savingsAccount1 = SavingsAccountFactory.createSavingsAccount("91112",555,"PR454", BigDecimal.valueOf(2000));
+    private static SystemDB systemDB = SystemDB.getSystemDB();
+
+    //To test singleton
+    private static SystemDB systemDB2 = SystemDB.getSystemDB();
+
+    public static void main(String[] args) throws Exception {
+        new SavingsAccountServiceTest().runAllSavingsAccountTests();
+    }
+
+    private void runAllSavingsAccountTests() throws Exception {
+        // Adding Accounts to database
+        systemDB.add(savingsAccount);
+        systemDB.add(savingsAccount1);
+
+        testServiceWithdraw();
+        testExceededBalance();
+        testWithdrawNotFound();
+        testSingleton();
+    }
+
+    private void testServiceWithdraw() throws Exception {
+        // Savings Account '123X5Y' will be found, balance will be decremented
+        try{
+            service.withdraw("123X5Y",BigDecimal.valueOf(4000));
+            System.out.println("Test Pass {Account with Account Number 2 Found and Successfully withdrawn.} ");
+        }
+        catch (Exception e){
+            System.out.println("Test Fail {Balance NOT withdrawn.}");
+        }
+    }
+
+    private void testExceededBalance(){
+        // Making balance exceed minimum limit, expecting WithdrawalAmountTooLargeException
+        try{
+            service.withdraw("PR454",BigDecimal.valueOf(2000));
+        }
+        catch (Exception e){
+            System.out.println("Test Pass {" + e.getMessage() + ".}");
+        }
+    }
+
+    private void testWithdrawNotFound() {
+        // Finding none existing account expecting AccountNotFoundException
+        try{
+            service.withdraw("9",BigDecimal.valueOf(2002));
+        }
+        catch (AccountNotFoundException e){
+            System.out.println("Test Pass {" + e.getMessage() + ".}");
+        }
+        catch (Exception err){
+            System.out.println("Test Fail {Account was found.}");
+        }
+    }
+
+    private void testSingleton(){
+        if(systemDB == systemDB2){
+            System.out.println("Test Pass {Singleton is successfully applied.}");
+        }
+        else{
+            System.out.println("Test Fail {Singleton is NOT applied.}");
+        }
+    }
+
+}
