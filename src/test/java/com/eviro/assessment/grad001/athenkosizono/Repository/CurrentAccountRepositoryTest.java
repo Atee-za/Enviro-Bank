57aaa9c4eec14b86dc74a78bea2b29e16be07df6
Athenkosi <athenkosizono@gmail.com>
2022-04-29 12:33:55 +0200

    Added Repository layer test cases for Savings and Current accounts

---
+++ src/test/java/com/eviro/assessment/grad001/athenkosizono/Repository/CurrentAccountRepositoryTest.java
@@ -0,0 +1,55 @@
+package com.eviro.assessment.grad001.athenkosizono.Repository;
+
+import com.eviro.assessment.grad001.athenkosizono.Domain.CurrentAccount;
+import com.eviro.assessment.grad001.athenkosizono.Domain.Database.SystemDB;
+import com.eviro.assessment.grad001.athenkosizono.Factory.CurrentAccountFactory;
+
+import java.math.BigDecimal;
+
+class CurrentAccountRepositoryTest {
+
+    public static void main(String[] args) throws Exception {
+        new CurrentAccountRepositoryTest().runAllSavingsAccountTests();
+    }
+
+    private static CurrentAccountRepository repository = CurrentAccountRepository.getCurrentAccountRepository();
+    private CurrentAccount currentAccount = CurrentAccountFactory.createCurrentAccount("1111",108,"123X5Y", BigDecimal.valueOf(5000), BigDecimal.valueOf(6000));
+    private CurrentAccount currentAccount1 = CurrentAccountFactory.createCurrentAccount("8544",150,"PR454", BigDecimal.valueOf(-2000), BigDecimal.valueOf(8000));
+    private static SystemDB systemDB = SystemDB.getSystemDB();
+
+    private void runAllSavingsAccountTests() throws Exception {
+        systemDB.add(currentAccount);
+        systemDB.add(currentAccount1);
+
+        currentAccountWithdraw();
+        testExceededBalance();
+        testWithdrawNotFound();
+    }
+
+    private void currentAccountWithdraw() throws Exception {
+        repository.withdraw("123X5Y", BigDecimal.valueOf(10000));
+        System.out.println("Test Pass {Current Account with account number 123X5Y successfully executed.}");
+    }
+
+    private void testExceededBalance(){
+        // Making balance exceed maximum overdraft limit, expecting WithdrawalAmountTooLargeException
+        try{
+            repository.withdraw("PR454",BigDecimal.valueOf(7000));
+        }
+        catch (Exception e){
+            System.out.println("Test Pass {" + e.getMessage() + ".}");
+        }
+    }
+
+    private void testWithdrawNotFound() {
+        // Finding none existing account expecting AccountNotFoundException
+        try{
+            repository.withdraw("XXX",BigDecimal.valueOf(2002));
+            System.out.println("Test Fail {Withdrawal was success.}");
+        }
+        catch (Exception e){
+            System.out.println("Test Pass {" + e.getMessage() + ".}");
+        }
+    }
+
+}
---
+++ src/test/java/com/eviro/assessment/grad001/athenkosizono/Repository/SavingsAccountRepositoryTest.java
@@ -0,0 +1,64 @@
+package com.eviro.assessment.grad001.athenkosizono.Repository;
+
+import com.eviro.assessment.grad001.athenkosizono.Domain.Database.SystemDB;
+import com.eviro.assessment.grad001.athenkosizono.Domain.SavingsAccount;
+import com.eviro.assessment.grad001.athenkosizono.Factory.SavingsAccountFactory;
+
+import javax.security.auth.login.AccountNotFoundException;
+import java.math.BigDecimal;
+
+class SavingsAccountRepositoryTest {
+
+    public static void main(String[] args) throws Exception {
+        //Run tests
+        new SavingsAccountRepositoryTest().runAllSavingsAccountTests();
+    }
+
+    private static SavingsAccountRepository repository = SavingsAccountRepository.getSavingsAccountRepository();
+    private SavingsAccount savingsAccount = SavingsAccountFactory.createSavingsAccount("1111",108,"123X5Y", BigDecimal.valueOf(5000));
+    private SavingsAccount savingsAccount1 = SavingsAccountFactory.createSavingsAccount("91112",555,"PR454", BigDecimal.valueOf(2000));
+    private static SystemDB systemDB = SystemDB.getSystemDB();
+
+    private void runAllSavingsAccountTests() throws Exception {
+        systemDB.add(savingsAccount);
+        systemDB.add(savingsAccount1);
+        testRepositoryWithdraw();
+        testExceededBalance();
+        testWithdrawNotFound();
+    }
+
+    private void testRepositoryWithdraw() throws Exception {
+        // Savings Account '123X5Y' will be found, balance will be decremented
+        try{
+            repository.withdraw("123X5Y",BigDecimal.valueOf(4000));
+            System.out.println("Test Pass {Account with Account Number 2 Found and Successfully withdrawn.} ");
+        }
+        catch (Exception e){
+            System.out.println("Test Fail {Balance NOT withdrawn.}");
+        }
+    }
+
+    private void testExceededBalance(){
+        // Making balance exceed minimum limit, expecting WithdrawalAmountTooLargeException
+        try{
+            repository.withdraw("PR454",BigDecimal.valueOf(2000));
+        }
+        catch (Exception e){
+            System.out.println("Test Pass {" + e.getMessage() + ".}");
+        }
+    }
+
+    private void testWithdrawNotFound() {
+        // Finding none existing account expecting AccountNotFoundException
+        try{
+            repository.withdraw("9",BigDecimal.valueOf(2002));
+        }
+        catch (AccountNotFoundException e){
+            System.out.println("Test Pass {" + e.getMessage() + ".}");
+        }
+        catch (Exception err){
+            System.out.println("Test Fail {Account was found.}");
+        }
+    }
+
+}
